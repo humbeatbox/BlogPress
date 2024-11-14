7402ef95ad61c9113716c9d0275a310568215eba
GaryChang <garychang1214@gmail.com>
2024-11-14 17:22:26 -0500

    feat(AS3P2) : add the route in server.js for upload the content

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const path = require("path");
 const ContentService = require("./content-service");
 const contentService = new ContentService();
 app.use(express.static("public")); //static files for "/css/main.css
+require("dotenv").config();
+
+//for Cloudinary
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+const upload = multer(); // No disk storage, files are stored in memory
 
 async function startServer() {
   try {
@@ -58,4 +73,47 @@ app.get("/categories", (req, res) => {
 //part 1, step 2
 app.get("/articles/add", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "addArticle.html"));
+});
+//part 2 step3
+//for post the article (provided by professor)
+app.post("/articles/add", upload.single("featureImage"), (req, res) => {
+  //have the file then upload to cloudinary
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) resolve(result);
+          else reject(error);
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      return result;
+    }
+
+    upload(req)
+      .then((uploaded) => {
+        processArticle(uploaded.url);
+      })
+      .catch((err) =>
+        res.status(500).json({ message: "Image upload failed", error: err })
+      );
+  } else {
+    //no file uploaded the ""
+    processArticle("");
+  }
+
+  function processArticle(imageUrl) {
+    req.body.featureImage = imageUrl;
+
+    contentService
+      .addArticle(req.body)
+      .then(() => res.redirect("/articles"))
+      .catch((err) =>
+        res.status(500).json({ message: "Article creation failed", error: err })
+      );
+  }
 });
